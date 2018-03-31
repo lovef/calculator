a4c502dcffdc4c845c232ff5c8c14a0d9b5a0f61
Love F <lovef.code@gmail.com>
2018-03-31 15:36:41 +0200

    Replace last operator

--- src/app/calculator/expression.spec.ts
+++ src/app/calculator/expression.spec.ts
@@ -7,57 +7,65 @@ describe('Expression', () => {
   })
 
   it('pressing 1, 2 and gives 12', () => {
-    var expression = new Expression()
+    let expression = new Expression()
     expression.push('1')
     expression.push('2')
     expect(expression.result).toBe(12);
   })
 
   it('pressing ,5 gives 0.5', () => {
-    var expression = new Expression()
+    let expression = new Expression()
     expression.push(',')
     expression.push('5')
     expect(expression.result).toBe(0.5)
   })
 
+  it('entering multiple operators replaces the last operator', () => {
+    let expression = new Expression()
+    expression.push('1*')
+    expect(expression.toString()).toBe('1 *')
+    expression.push('+')
+    expect(expression.toString()).toBe('1 +')
+  })
+
   it('1+1 gives the expression string "1 + 1"', () => {
-    var expression = new Expression()
+    let expression = new Expression()
     expression.push('1+1')
     expect(expression.toString()).toBe('1 + 1')
   })
 
   it('1 + 1 = 2', () => {
-    var expression = new Expression()
+    let expression = new Expression()
     expression.push('1 + 1')
     expect(expression.result).toBe(2);
   })
 
   it('1 + 1 - 1 = 1', () => {
-    var expression = new Expression()
+    let expression = new Expression()
     expression.push('1 + 1 - 1')
     expect(expression.result).toBe(1);
   })
 
   it('2 * 3 = 6', () => {
-    var expression = new Expression()
+    let expression = new Expression()
     expression.push('2 * 3')
     expect(expression.result).toBe(6);
   })
 
   it('6 / 3 = 2', () => {
-    var expression = new Expression()
+    let expression = new Expression()
     expression.push('6 / 3')
     expect(expression.result).toBe(2);
   })
 
   it('1 + 2 * 3 = 7', () => {
-    var expression = new Expression()
+    let expression = new Expression()
     expression.push('1 + 2 * 3')
     expect(expression.result).toBe(7);
   })
 
   it('press backspace to erase input', () => {
-    var expression = new Expression()
+    let expression = new Expression()
     expression.push('1,2 + 3')
     expect(expression.toString()).toBe('1,2 + 3')
     expression.backspace()
